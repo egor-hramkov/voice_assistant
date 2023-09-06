efbd1679eab3fd190ce33255dfabf6aabe86accc
Egor Khramkov <e.hramkov@ritg.ru>
2023-09-06 13:04:02 +0400

    Настройка конфига

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
 import speech_recognition as sr
 
-microphones = sr.Microphone.list_microphone_names()
-for index, name in enumerate(microphones):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+assistant_name = ''
+assistant_sex = 'female'
+assistant_language = 'ru'
+recognition_language = "ru-RU"
+device_index = 1
+
+
+def get_microphone_list():
+    microphones = sr.Microphone.list_microphone_names()
+    for index, name in enumerate(microphones):
+        print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+
+if __name__ == "__main__":
+    get_microphone_list()
